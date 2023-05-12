2aae36ea380db1012064bbc7b0a3311d8cc127cd
Tyler Feist <tfeist612@gmail.com>
2023-05-11 20:02:37 -0600

    add treesitter parsers

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,5 +1,27 @@
 local plugins = {
 
+  -- custom parser list 
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "bash",
+        "python",
+        "go",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "json",
+        "proto",
+        "dockerfile",
+      },
+    },
+  },
+
+  -- install vim fugitive
   {
     "tpope/vim-fugitive",
     lazy = false,
