6ee21f89c5da3b60e84a8221a7a08ba731b0716f
Tyler Feist <tfeist612@gmail.com>
2023-05-11 21:40:32 -0600

    setup lsp and formatter

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,22 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = {
+-- lsp 
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  "lua_ls",
+  "bashls",
+  "pyright",
+  "gopls",
+  "html",
+  "cssls",
+  "denols",
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,11 +1,49 @@
 local plugins = {
 
+  -- install lsp servers and formatters
+  {
+   "williamboman/mason.nvim",
+   opts = {
+      ensure_installed = {
+        -- lsp
+        "lua-language-server",
+        "bash-language-server",
+        "pyright",
+        "gopls",
+        "html-lsp",
+        "css-lsp",
+        "deno",
+        -- format 
+        "stylua",
+        "gofumpt",
+        "black",
+        "prettier",
+      },
+    },
+  },
+
+  -- setup lsp for installed servers 
+  {
+    "neovim/nvim-lspconfig",
+
+     dependencies = {
+       "jose-elias-alvarez/null-ls.nvim",
+       config = function()
+         require "custom.configs.null-ls"
+       end,
+     },
+
+     config = function()
+        require "plugins.configs.lspconfig"
+        require "custom.configs.lspconfig"
+     end,
+  },
+
   -- custom parser list 
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "vim",
         "lua",
         "bash",
         "python",
@@ -15,6 +53,7 @@ local plugins = {
         "javascript",
         "typescript",
         "json",
+        "yaml",
         "proto",
         "dockerfile",
       },
